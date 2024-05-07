8095c32bb4d903ad1eeaed972e4119aa93ba9c68
EKATERINA_ZAKHAROVA <93439877+Flowruns@users.noreply.github.com>
2024-05-07 21:17:07 +0700

    Managing form state

---
+++ YouTubeForm.tsx
@@ -0,0 +1,48 @@
+﻿import React from 'react';
+import {useForm} from "react-hook-form";
+
+
+// Компонент ютуб формы
+export const YouTubeForm = () => {
+    
+    // Используем хук из библиотеки  react-hook-form
+    const form = useForm()
+    
+    // Регистрируем элемент управления формы с помощью хука useForm
+    const {register} = form
+   
+    // Возвращаем JSX, который содержит структуру компонента формы
+    return (
+        <div>
+            <h1>YouTube форма</h1>
+
+            <form>
+                <label htmlFor="username">Имя пользователя</label>
+                <input 
+                    type="text" 
+                    id="username"
+                    {...register("userName")}
+                />
+
+                <label htmlFor="email">Почта</label>
+                <input 
+                    type="email" 
+                    id="email"
+                    {...register("email")}
+                />
+
+                <label htmlFor="channel">Канал</label>
+                <input 
+                    type="text" 
+                    id="channel"
+                    {...register("channel")}
+                />
+
+                <button>Отправить</button>
+            </form>
+        </div>
+    );
+};
+
+// Экспортируем компонент YouTubeForm
+export default YouTubeForm;
